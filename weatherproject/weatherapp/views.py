5912231a5432f8392c114d6a730f6afc367fbfd1
Samuel <samuelayenitaju@gmail.com>
2026-02-12 18:39:50 +0100

    feat: Implement weather data retrieval and update home page template

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+import datetime
+import requests
 
 def home(request):
-    return render(request, 'weatherapp/home.html')
+
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else: 
+        city = 'lagos'
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=bf7d64842ff40f509e79d234058c593e'
+
+    PARAMS = {'units': 'metric'}
+
+    data = requests.get(url, PARAMS).json()
+
+    description = data['weather'][0]['description']
+    icon = data['weather'][0]['icon']
+    temp = data['main']['temp']
+
+    day = datetime.date.today()
+
+    return render(request, 'weatherapp/home.html',{'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city': city})
