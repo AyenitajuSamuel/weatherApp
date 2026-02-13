60be23b75aa8d441b0360f94a1e0507c152568fd
Samuel <samuelayenitaju@gmail.com>
2026-02-13 12:26:22 +0100

    feat: Enhance home page layout and functionality with weather details and improved styling

--- weatherproject/weatherapp/views.py
+++ weatherproject/weatherapp/views.py
@@ -1,24 +1,48 @@
 from django.shortcuts import render
+from django.contrib import messages
 import datetime
 import requests
 
 def home(request):
 
-    if 'city' in request.POST:
-        city = request.POST['city']
-    else: 
-        city = 'lagos'
+    city = request.POST.get('city', 'lagos')
 
-    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=bf7d64842ff40f509e79d234058c593e'
+    url = f'https://api.openweathermap.org/data/2.5/weather'
 
-    PARAMS = {'units': 'metric'}
+    params = {
+        'q': city,
+        'appid': 'bf7d64842ff40f509e79d234058c593e',
+        'units': 'metric'
+    }
 
-    data = requests.get(url, PARAMS).json()
+    data = requests.get(url, params=params).json()
+
+    if data.get("cod") != 200:
+        messages.error(request, "City not found. Showing Lagos instead.")
+
+        city = "lagos"
+        params['q'] = city
+        response = requests.get(url, params=params)
+        data = response.json()
 
     description = data['weather'][0]['description']
     icon = data['weather'][0]['icon']
     temp = data['main']['temp']
-
+    feel_temp = data['main']['feels_like']
+    humidity = data['main']['humidity']
+    windspeed = data['wind']['speed']
+    pressure = data['main']['pressure']
     day = datetime.date.today()
 
-    return render(request, 'weatherapp/home.html',{'description': description, 'icon': icon, 'temp': temp, 'day': day, 'city': city})
+    context = {'description': description,
+            'icon': icon,
+            'temp': temp,
+            'feel_temp': feel_temp, 
+            'day': day, 
+            'city': city, 
+            'humidity': humidity, 
+            'windspeed': windspeed,
+            'pressure': pressure,
+            }
+
+    return render(request, 'weatherapp/home.html', context)
